cc961918f92e32b3e3c26cc306e8a1b058019483
rgreb26 <71271173+rgreb26@users.noreply.github.com>
2025-01-16 07:52:20 -0600

    Update explorer.js

--- programs/explorer/explorer.js
+++ programs/explorer/explorer.js
@@ -277,24 +277,6 @@ var resolve_address = async function (address) {
 	var is_url = !!address.match(/\w+:\/\//);
 	var drive_match = address.match(/^\(?([a-z]):\)?[/\\]?\)?$/i);
 	var zone = "unknown";
-	function handle_url_case() {
-		if (!address.match(/^https?:\/\/web.archive.org\//) && !address.startsWith(window.location.origin)) {
-			// special exemption: show archive but later version
-			if (address.match(/^https?:\/\/(www\.)?(windows93.net)/)) {
-				address = "https://web.archive.org/web/2015-05-05/" + address;
-			// complete exemptions:
-			} else if (
-				!address.match(/^https?:\/\/(www\.)?(copy.sh|topotech.github.io\/interdimensionalcable|isaiahodhner.io|brie.fi\/ng)/) &&
-				!address.match(/^(file|data|blob):\/\//)
-			) {
-				address = "https://web.archive.org/web/1998/" + address;
-			}
-		}
-		is_url = true;
-		// zone = address.startsWith(window.location.origin) ? "local" : "internet"; // @TODO
-		zone = "internet";
-		return { normalized_address: address, is_url, zone };
-	}
 	if (system_folder_lowercase_name_to_path[address.toLowerCase()]) {
 		address = system_folder_lowercase_name_to_path[address.toLowerCase()];
 		zone = "local";
